a317a849656433cf8f9877b29ca5cae6e1ba9437
Griggorii <40373174+Griggorii@users.noreply.github.com>
2021-10-17 23:14:29 +0800

    Add files via upload

--- Nemo/Setting_dconf_linux_OS21.04_V52.0_By_Griggorii_Wayland_vertical_adaptation.sh
+++ Nemo/Setting_dconf_linux_OS21.04_V52.0_By_Griggorii_Wayland_vertical_adaptation.sh
@@ -10606,6 +10606,115 @@ rm ./daemon.conf
 EOF
 sudo rm ./daemon.conf
 EOF
+sudo ln -s /lib/linux-sound-base/noOSS.modprobe.conf /lib/modprobe.d/blacklist-oss.conf
+EOF
+sudo rm /lib/modprobe.d/alsa-base.conf
+EOF
+sudo rm /usr/lib/modprobe.d/alsa-base.conf
+EOF
+cat << EOF > alsa-base.conf
+# autoloader aliases
+install sound-slot-0 /sbin/modprobe snd-card-0
+install sound-slot-1 /sbin/modprobe snd-card-1
+install sound-slot-2 /sbin/modprobe snd-card-2
+install sound-slot-3 /sbin/modprobe snd-card-3
+install sound-slot-4 /sbin/modprobe snd-card-4
+install sound-slot-5 /sbin/modprobe snd-card-5
+install sound-slot-6 /sbin/modprobe snd-card-6
+install sound-slot-7 /sbin/modprobe snd-card-7
+
+# Cause optional modules to be loaded above generic modules
+install snd /sbin/modprobe --ignore-install snd $CMDLINE_OPTS && { /sbin/modprobe --quiet --use-blacklist snd-ioctl32 ; /sbin/modprobe --quiet --use-blacklist snd-seq ; }
+#
+# Workaround at bug #499695 (reverted in Ubuntu see LP #319505)
+install snd-pcm /sbin/modprobe --ignore-install snd-pcm $CMDLINE_OPTS && { /sbin/modprobe --quiet --use-blacklist snd-pcm-oss ; : ; }
+install snd-mixer /sbin/modprobe --ignore-install snd-mixer $CMDLINE_OPTS && { /sbin/modprobe --quiet --use-blacklist snd-mixer-oss ; : ; }
+install snd-seq /sbin/modprobe --ignore-install snd-seq $CMDLINE_OPTS && { /sbin/modprobe --quiet --use-blacklist snd-seq-midi ; /sbin/modprobe --quiet --use-blacklist snd-seq-oss ; : ; }
+#
+install snd-rawmidi /sbin/modprobe --ignore-install snd-rawmidi $CMDLINE_OPTS && { /sbin/modprobe --quiet --use-blacklist snd-seq-midi ; : ; }
+# Cause optional modules to be loaded above sound card driver modules
+install snd-emu10k1 /sbin/modprobe --ignore-install snd-emu10k1 $CMDLINE_OPTS && { /sbin/modprobe --quiet --use-blacklist snd-emu10k1-synth ; }
+install snd-via82xx /sbin/modprobe --ignore-install snd-via82xx $CMDLINE_OPTS && { /sbin/modprobe --quiet --use-blacklist snd-seq ; }
+
+# Load saa7134-alsa instead of saa7134 (which gets dragged in by it anyway)
+install saa7134 /sbin/modprobe --ignore-install saa7134 $CMDLINE_OPTS && { /sbin/modprobe --quiet --use-blacklist saa7134-alsa ; : ; }
+# Prevent abnormal drivers from grabbing index 0
+options bt87x index=-2
+options cx88_alsa index=-2
+options saa7134-alsa index=-2
+options snd-atiixp-modem index=-2
+options snd-intel8x0m index=-2
+options snd-via82xx-modem index=-2
+options snd-usb-audio index=-2
+options snd-usb-caiaq index=-2
+options snd-usb-ua101 index=-2
+options snd-usb-us122l index=-2
+options snd-usb-usx2y index=-2
+# Ubuntu #62691, enable MPU for snd-cmipci
+options snd-cmipci mpu_port=0x330 fm_port=0x388
+# Keep snd-pcsp from being loaded as first soundcard
+options snd-pcsp index=-2
+# Keep snd-usb-audio from beeing loaded as first soundcard
+options snd-usb-audio index=-2
+EOF
+sudo cp ./alsa-base.conf /usr/lib/modprobe.d/
+EOF
+sudo cp alsa-base.conf /usr/lib/modprobe.d/
+EOF
+sudo cp ./alsa-base.conf /lib/modprobe.d/
+EOF
+sudo cp alsa-base.conf /lib/modprobe.d/
+EOF
+cp ./alsa-base.conf /usr/lib/modprobe.d/
+EOF
+cp alsa-base.conf /usr/lib/modprobe.d/
+EOF
+cp ./alsa-base.conf /lib/modprobe.d/
+EOF
+cp alsa-base.conf /lib/modprobe.d/
+EOF
+rm ./alsa-base.conf
+EOF
+sudo rm /lib/modprobe.d/aliases.conf
+EOF
+sudo rm /usr/lib/modprobe.d/aliases.conf
+EOF
+cat << EOF > aliases.conf
+# These are the standard aliases and dependencies.
+# This file does not need to be modified.
+
+# prevent unusual drivers from appearing as the first sound device ###########
+options snd-pcsp index=-2
+options snd-usb-audio index=-2
+options cx88_alsa index=-2
+options snd-atiixp-modem index=-2
+options snd-intel8x0m index=-2
+options snd-via82xx-modem index=-2
+
+# work around other kernel issues ############################################
+# The EHCI driver should be loaded before the ones for low speed controllers
+# or some devices may be confused when they are disconnected and reconnected.
+softdep uhci-hcd pre: ehci-hcd
+softdep ohci-hcd pre: ehci-hcd
+EOF
+sudo cp ./aliases.conf /usr/lib/modprobe.d/
+EOF
+sudo cp aliases.conf /usr/lib/modprobe.d/
+EOF
+sudo cp ./aliases.conf /lib/modprobe.d/
+EOF
+sudo cp aliases.conf /lib/modprobe.d/
+EOF
+cp ./aliases.conf /usr/lib/modprobe.d/
+EOF
+cp aliases.conf /usr/lib/modprobe.d/
+EOF
+cp ./aliases.conf /lib/modprobe.d/
+EOF
+cp aliases.conf /lib/modprobe.d/
+EOF
+rm ./aliases.conf
+EOF
 sudo mkdir /etc/xdg
 EOF
 sudo mkdir /etc/xdg/menus
