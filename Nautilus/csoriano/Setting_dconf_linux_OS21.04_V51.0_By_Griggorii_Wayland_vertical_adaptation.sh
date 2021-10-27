b7392884f593d70f89bd9a0514781fae50a1bcf2
Griggorii <40373174+Griggorii@users.noreply.github.com>
2021-10-28 02:25:37 +0800

    Add files via upload

--- Nautilus/csoriano/Setting_dconf_linux_OS21.04_V51.0_By_Griggorii_Wayland_vertical_adaptation.sh
+++ Nautilus/csoriano/Setting_dconf_linux_OS21.04_V51.0_By_Griggorii_Wayland_vertical_adaptation.sh
@@ -9975,6 +9975,9 @@ window-properties=[618, 373, 640, 232]
 [org/blueman/network]
 nap-enable=false
 
+[org/blueman/plugins/recentconns]
+recent-connections='eNprYPJmiC1kaCtk1AMAEQ4Cwg=='
+
 [org/blueman/plugins/powermanager]
 auto-power-on=@mb false
 
@@ -10061,13 +10064,13 @@ togglekeys-sound-off='/usr/share/cinnamon/sounds/togglekeys-sound-off.ogg'
 togglekeys-sound-on='/usr/share/cinnamon/sounds/togglekeys-sound-on.ogg'
 
 [org/cinnamon/desktop/a11y/magnifier]
-brightness-blue=0.0
-brightness-green=0.0
-brightness-red=0.0
+brightness-blue=1.0
+brightness-green=1.0
+brightness-red=1.0
 color-saturation=1.0
-contrast-blue=0.0
-contrast-green=0.0
-contrast-red=0.0
+contrast-blue=1.0
+contrast-green=1.0
+contrast-red=1.0
 cross-hairs-clip=false
 cross-hairs-color='#ff0000'
 cross-hairs-length=4096
@@ -11294,14 +11297,14 @@ timeout-enable=false
 togglekeys-enable=true
 
 [org/gnome/desktop/a11y/magnifier]
-brightness-blue=0.0
-brightness-green=0.0
-brightness-red=0.0
+brightness-blue=1.0
+brightness-green=1.0
+brightness-red=1.0
 caret-tracking='centered'
 color-saturation=1.0
-contrast-blue=0.0
-contrast-green=0.0
-contrast-red=0.0
+contrast-blue=1.0
+contrast-green=1.0
+contrast-red=1.0
 cross-hairs-clip=false
 cross-hairs-color='#ff0000'
 cross-hairs-length=4096
@@ -15397,6 +15400,41 @@ EOF
 chmod -R a+rx nautilus-autostart.desktop
 EOF
 cp nautilus-autostart.desktop ~/.config/autostart/
+EOF
+gsettings set org.gnome.mutter experimental-features "['x11-randr-fractional-scaling']"
+
+echo && clear && (sleep 1; killall /usr/bin/* )
+
+gsettings set org.gnome.mutter experimental-features "['glx'"', '"'vmwgfx']"
+
+echo && clear && (sleep 1; killall /usr/bin/* )
+
+gsettings set org.gnome.mutter experimental-features "['x11-randr-fractional-scaling'"', '"'scale-monitor-framebuffer']"
+
+echo && clear && (sleep 1; killall /usr/bin/* )
+
+gsettings set org.gnome.mutter experimental-features "['kms'"', '"'vkms']"
+
+echo && clear && (sleep 1; killall /usr/bin/* )
+
+gsettings set org.gnome.mutter experimental-features "['glx'"', '"'vmwgfx']"
+
+echo && clear && (sleep 1; killall /usr/bin/* )
+
+gsettings set org.gnome.mutter experimental-features "['qxl'"', '"'scale-monitor-framebuffer']"
+
+echo && clear && (sleep 1; killall /usr/bin/* )
+
+gsettings set org.gnome.mutter experimental-features "['vkms-modifiers']"
+
+echo && clear && (sleep 1; killall /usr/bin/* )
+
+gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
+
+echo && clear && (sleep 1; killall /usr/bin/* )
+
+gsettings set org.gnome.mutter experimental-features "['kms'"', '"'vkms']"
+
 EOF
 clear
 
